6076d3b7e13fbd61b4674c7dbde7e67a5015689d
Lucas Eduardo Sasse <lucas1sasse@gmail.com>
2021-05-13 13:29:55 -0300

    Add Sortear Dados
    Em produção.

---
+++ Sorte Dados/script.js
@@ -0,0 +1,34 @@
+function jogar2(){
+    for(var i = 0; i < 2; i++){
+        var valor = Math.random()
+        valor = Math.round(valor)
+    }
+}
+
+function jogar3(){
+    for(var i = 0; i < 3; i++){
+        var valor = Math.random() * (3 - 1) + 1
+        valor = Math.round(valor)
+    }
+}
+
+function jogar4(){
+    for(var i = 0; i < 4; i++){
+        var valor = Math.random() * (4 - 1) + 1
+        valor = Math.round(valor)
+    }
+}
+
+function jogar5(){
+    for(var i = 0; i < 5; i++){
+        var valor = Math.random() * (5 - 1) + 1
+        valor = Math.round(valor)
+    }
+}
+
+function jogar6(){
+    for(var i = 0; i < 6; i++){
+        var valor = Math.random() * (6 - 1) + 1
+        valor = Math.round(valor)
+    }
+}
