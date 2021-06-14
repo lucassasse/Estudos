7e284b6c6ab34407d8fd9264583f055038718e74
Lucas Eduardo Sasse <lucas1sasse@gmail.com>
2021-06-14 19:57:22 -0300

    Create a localStorage study

---
+++ trabalho_com_localStorage/index.js
@@ -0,0 +1,51 @@
+//metodo para armazenar valores = setItem
+//metodo para resgatar valores = getItem
+//metodo para remover valores = removeItem
+//metodo para remover TODOS os valores = clear
+
+//numeroJSON = JSON.parse(numero) = converte valor do local storage em número
+
+
+//Exercício 01:
+
+window.localStorage.setItem('numero', '0')
+let resultadoNumero = document.getElementById('numeroAdicionar')
+
+function converterNumero(){
+    let numero = window.localStorage.getItem('numero')
+    return numeroJSON = JSON.parse(numero)
+}
+
+function adicionarNumero(){
+    numeroConvertido = converterNumero()
+    numeroConvertido += 1
+    resultadoNumero.innerHTML = numeroConvertido
+    window.localStorage.setItem('numero', numeroConvertido)
+}
+
+
+//Exercício 02:
+
+let resultadoTexto = document.getElementById('textoAdicionar')
+
+function capturarTexto(){
+    let valorTexto = document.getElementById('textoEnviado').value
+    window.localStorage.setItem('texto', valorTexto)
+}
+
+function ResgatarTexto(){
+    let texto = window.localStorage.getItem('texto')
+    resultadoTexto.innerHTML = texto
+}
+
+
+//Exercício 03:
+
+function limparValores(){
+    //localStorage.clear()
+    window.localStorage.removeItem('numero')
+    window.localStorage.removeItem('texto')
+    numeroJSON = 0
+    resultadoNumero.innerHTML = ''
+    resultadoTexto.innerHTML = ''
+}
