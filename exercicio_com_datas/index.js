28a79665eda7979f8412db65d21ef546eef216be
Lucas Eduardo Sasse <lucas1sasse@gmail.com>
2021-06-15 16:38:09 -0300

    update

--- exercicio_com_datas/index.js
+++ exercicio_com_datas/index.js
@@ -1,65 +1,16 @@
-const campoData = document.getElementById("date")
+let campoData = document.getElementById("date")
+resultado = document.getElementById('resultado')
+
+let dataAtual = new Date()
+const diaAtual = dataAtual.getDate()
+const mesAtual = dataAtual.getMonth()+1
+
 
 function verificarData(){
     valorData = campoData.value
 
-    dataAtual()
-}
-
-function dataAtual(){
-    const data = new Date()
-    const diaAtual = data.getDate()
-    const mesAtual = data.getMonth()+1
-    console.log(diaAtual)
-    console.log(mesAtual)
-}
 
 
-
-/*
-var resposta = document.getElementById('resposta');
-let dia;
-let mes;
-let estacao;
-
-function captarValores(){
-    dia = document.getElementById('dia').value;
-    mes = document.getElementById('mes').value;
-    verificarData();
-}
-
-function verificarData(){
-    if(dia < 1 || dia > 31 || mes < 1 || mes > 12){
-        alert('Digite uma data válida...');
-    }
-    else if(mes == 1 || mes == 2){
-        estacao = "Verão"
-    } else if(mes == 3){
-        if(dia < 20){
-            estacao = "Verão"
-        } else{
-            estacao = "Outono"
-        }
-    } else if(mes == 4 || mes == 5){
-        estacao = "Outono"
-    } else if(mes == 6){
-        if(dia < 20){
-            estacao = "Outono"
-        } else{
-            estacao = "Inverno"
-        }
-    } else if(mes == 7 || mes == 8){
-        estacao = "Inverno"
-    } else if(mes == 9){
-        if(dia < 21){
-            estacao = "Inverno"
-        } else{
-            estacao = "Primavera"
-        }
-    } else if(mes == 10 || mes == 11){
-        estacao = "Primavera"
-    } else{
-        estacao = "Primavera"
-    }
-    alert(estacao)
+    resultado.innerHTML = `Você nasceu no ${dataAtual}`
+    console.log(valorData)
 }
