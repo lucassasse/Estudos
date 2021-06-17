eee87c9740acf1b19334ede828b0b8604917f41c
Lucas Eduardo Sasse <lucas1sasse@gmail.com>
2021-06-17 14:22:20 -0300

    update

--- sorteador_de_dados/script.js
+++ sorteador_de_dados/script.js
@@ -1,5 +1,13 @@
 let resultado = document.getElementById('valor')
 let dados = []
+let imagens = [
+    'Imagens/dado01.png',
+    'Imagens/dado02.png',
+    'Imagens/dado03.png',
+    'Imagens/dado04.png',
+    'Imagens/dado05.png',
+    'Imagens/dado06.png',
+]
 
 function gerarNumeros(){
     let resultado = Math.random() * (6 - 1) + 1
@@ -8,8 +16,8 @@ function gerarNumeros(){
 }
 
 function quantidadeDados(valor){
-    let caracter = parseInt(valor.value)
     limparArray()
+    let caracter = parseInt(valor.value)
     for(let i = caracter; i > 0; i--){
         gerarNumeros()
     }
