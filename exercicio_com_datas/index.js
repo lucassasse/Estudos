a5a7e43f3bf03c96e8cd48d551075d58f327d6e8
Lucas Eduardo Sasse <lucas1sasse@gmail.com>
2021-07-06 21:47:46 -0300

    bug fixed

--- exercicio_com_datas/index.js
+++ exercicio_com_datas/index.js
@@ -2,15 +2,17 @@ let campoData = document.getElementById("date")
 resultado = document.getElementById('resultado')
 
 let dataAtual = new Date()
-const diaAtual = dataAtual.getDate()
-const mesAtual = dataAtual.getMonth()+1
-
+let diaAtual = dataAtual.getDate()
+let mesAtual = dataAtual.getMonth()+1
 
 function verificarData(){
     valorData = campoData.value
+    let valorDataData = new Date(valorData)
 
+    let diaData = valorDataData.getDate()
+    let mesData = valorDataData.getMonth()+1
 
-
-    resultado.innerHTML = `Você nasceu no ${dataAtual}`
+    resultado.innerHTML = `Você nasceu no ${diaData} e ${mesData}`
     console.log(valorData)
+    console.log(dataAtual)
 }
