9f6882b668e11e4b48bcf62efc87cc04bbe8645f
Lucas Eduardo Sasse <lucas1sasse@gmail.com>
2021-07-13 11:07:00 -0300

    bug fixed in calculadora

--- calculadora_simples/index.js
+++ calculadora_simples/index.js
@@ -1,26 +1,3 @@
-/*var primeiroValor = parseFloat(prompt("Digite o primeiro valor:"))
-
-var operacao = prompt("digite a sua operação: ")
-
-var segundoValor = parseFloat(prompt("Digite o segundo valor:"))
-
-if(operacao == "adicao"){
-    var resultado = primeiroValor + segundoValor
-} else if(operacao == "-"){
-    var resultado = primeiroValor - segundoValor
-} else if(operacao == "*" || operacao == "x"){
-    var resultado = primeiroValor * segundoValor
-} else if(operacao == "/"){
-    var resultado = primeiroValor / segundoValor
-} else{
-    document.write("<h2>" + "Operação inválida" + "</h2>")
-}
-
-document.write("<h2>" + primeiroValor + " " + operacao + " " + segundoValor + " = " + resultado + "</h2>")
-
-Poderia/ deveria colocar o resultado dentro de cada condicional - if*/
-
-
 function calcularResultado(){
 var operacao = document.getElementById("funcao").value
     if(operacao == "+"){
@@ -31,9 +8,7 @@ var operacao = document.getElementById("funcao").value
         var valor = parseFloat(document.getElementById("iptPrimeiroValor").value) * parseFloat(document.getElementById("iptSegundoValor").value)
     } else if(operacao == "/"){
         var valor = parseFloat(document.getElementById("iptPrimeiroValor").value) / parseFloat(document.getElementById("iptSegundoValor").value)
-    }/* else{
-        document.write("<h2>" + "Operação inválida" + "</h2>")
-    }*/
+    }
     return valor.toFixed(2);
 }
 
