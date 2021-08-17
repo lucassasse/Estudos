33343d3147766ede706d1269f1254d0f464b962e
Lucas Eduardo Sasse <lucas1sasse@gmail.com>
2021-08-17 19:12:08 -0300

    update

--- genius_game/index.js
+++ genius_game/index.js
@@ -1,99 +1,86 @@
-let campoVerde = document.getElementById('verde');
-let campoVermelho = document.getElementById('vermelho');
-let campoAzul = document.getElementById('azul');
-let campoAmarelo = document.getElementById('amarelo');
-let campoPontuacao = document.getElementById('pontuacao');
-
-//const campoCor = [campoVerde, campoVermelho, campoAmarelo, campoAzul];
+let campoVerde = document.getElementById("verde");
+let campoVermelho = document.getElementById("vermelho");
+let campoAzul = document.getElementById("azul");
+let campoAmarelo = document.getElementById("amarelo");
+let campoPontuacao = document.getElementById("pontuacao");
 
 let jogoAtual = [];
 let tentativa = [];
 let contador = 0;
-let resultadoTeste = 'false'
+let resultadoTeste = "false";
 
-function play(){
-    if(tentativa.length < jogoAtual.length){
-        alert("Primeiro tente adivinhar a sequência!\nForça, você está no caminho certo!")
-    } else {
-        contador ++;
-        sortearCor();
-        pontuacao();
+const valoresCampo = {
+  0: campoVerde,
+  1: campoVermelho,
+  2: campoAzul,
+  3: campoAmarelo,
+};
+
+function play() {
+  if (tentativa.length < jogoAtual.length) {
+    alert(
+      "Primeiro tente adivinhar a sequência!\nForça, você está no caminho certo!"
+    );
+  } else {
+    contador++;
+    sortearCor();
+    pontuacao();
+  }
+}
+
+function sortearCor() {
+  jogoAtual.push(Math.floor(Math.random() * 4));
+  console.log(jogoAtual);
+
+  setTimeout(() => {
+    chamaPiscar(0);
+  }, 300);
+}
+
+function chamaPiscar(pos) {
+  piscar(pos);
+
+  setTimeout(() => {
+    if (jogoAtual.length - 1 > pos) {
+      chamaPiscar(pos + 1);
     }
+  }, 800);
 }
 
-function sortearCor(){
-    jogoAtual.push(Math.floor(Math.random() * 1));
-    console.log(jogoAtual);
-    
-    setTimeout(() => {
-        chamaPiscar();
-    },300)
+function piscar(pos) {
+  valoresCampo[jogoAtual[pos]].classList.add("pisca");
+
+  setTimeout(() => {
+    valoresCampo[jogoAtual[pos]].classList.remove("pisca");
+  }, 600);
 }
 
-let pos = 0;
-function chamaPiscar(){
-    piscar(pos);
-
-    setTimeout(() => {
-        if(jogoAtual.length > pos){
-            chamaPiscar(pos);
-            pos++;
-        } else{
-            pos = 0;
-        }
-    }, 600)
-}
-
-async function piscar(pos){
-
-    if(jogoAtual[pos] === 0){
-        campoVerde.classList.add('pisca');
-    } else if(jogoAtual[pos] === 1){
-        campoVermelho.classList.add('pisca');
-    } else if(jogoAtual[pos] === 2){
-        campoAzul.classList.add('pisca');
-    } else{
-        campoAmarelo.classList.add('pisca');
+function selecionarCor(valor) {
+  if (jogoAtual.length <= tentativa.length) {
+    alert("Que tal tentar iniciar o jogo primeiro?");
+  } else {
+    tentativa.push(parseInt(valor.value));
+    if (jogoAtual.length === tentativa.length) {
+      resultadoTeste = tentativa.join() === jogoAtual.join();
+      verificarTentativa();
     }
-
-    await setTimeout(() => {
-        if(jogoAtual[pos] === 0){
-            campoVerde.classList.remove('pisca');
-        } else if(jogoAtual[pos] === 1){
-            campoVermelho.classList.remove('pisca');
-        } else if(jogoAtual[pos] === 2){
-            campoAzul.classList.remove('pisca');
-        } else{
-            campoAmarelo.classList.remove('pisca');
-        }
-    }, 600)
+  }
 }
 
-function selecionarCor(valor){
-    if(jogoAtual.length <= tentativa.length){
-        alert("Que tal tentar iniciar o jogo primeiro?");
-    } else{
-        tentativa.push(parseInt(valor.value));
-        if(jogoAtual.length === tentativa.length){
-            resultadoTeste = tentativa.join() === jogoAtual.join();
-            verificarTentativa();
-        }
-    }  
+function verificarTentativa() {
+  //Verifica somente após clicar em todos (implementar verificar clique a clique);
+  resultadoTeste ? play() : resetar();
+  tentativa = [];
+  console.log(jogoAtual);
 }
 
-function verificarTentativa(){ //Verifica somente após clicar em todos (implementar verificar clique a clique);
-    resultadoTeste ? play() : resetar();
-    tentativa = [];
-    console.log(jogoAtual);
+function resetar() {
+  jogoAtual = [];
+  tentativa = [];
+  contador = 0;
+  alert("Lamento, você perdeu!");
 }
 
-function resetar(){
-    jogoAtual = [];
-    tentativa = [];
-    contador = 0;
-    alert("Lamento, você perdeu!");
-}
-
-function pontuacao(){
-    campoPontuacao.innerHTML = "Pontuação: " + (contador-1);
+function pontuacao() {
+  campoPontuacao.innerHTML = "Pontuação: " + (contador - 1);
 }
