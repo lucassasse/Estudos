ecb5365a1eca88fdf05aa177bda1c3ce354a5152
Lucas Eduardo Sasse <lucas1sasse@gmail.com>
2024-01-01 20:10:56 -0300

    create a blackjack project

--- blackjack/src/App.vue
+++ blackjack/src/App.vue
@@ -18,7 +18,7 @@
 <input v-model="betValue">
 <button @click="initialCards">Jogar</button>
 <br><br>
-<button @click="pickCard">Pegar</button>
+<button @click="pickCard()">Pegar</button>
 <button @click="pass">Passar</button>
 </template>
 
@@ -32,9 +32,11 @@ export default {
       betValue: 1, //1
       inGame: false,
       baseUrl: 'https://www.deckofcardsapi.com/api/deck/',
-      deckId: 'mbar4yujsiai',
+      deckId: 'drik4k1u6uy7',
+      valueDealerCards: 0,
+      valuePlayerCards: 0,
+      playerCards: [],
       dealerCards: [],
-      playerCards: []
     }
   },
   methods:{
@@ -43,11 +45,13 @@ export default {
       await axios.get(url)
       .then(resp => {
         this.deckId = resp.data.deck_id
+        console.log(`Um novo deck foi gerado: ${this.deckId}`);
       })
       .catch(error => {
         console.log("[ERROR] = " + error)
         alert("Ops, algo de errado aconteceu.")
       })
+      this.inGame = false
     },
     async initialCards(){
       if(!this.inGame && this.chipsVerify()){
@@ -70,13 +74,13 @@ export default {
       const url = `${this.baseUrl}${this.deckId}/draw/?count=1`
       await axios.get(url)
       .then(resp => {
-        console.log(resp.data)
         this.playerCards.push(resp.data.cards[0])
       })
       .catch(error => {
         console.log("[ERROR] = " + error)
         alert("Ops, algo de errado aconteceu.")
       })
+      this.verifyEndGame()
       this.dealerTurn()
     },
     pass(){
@@ -91,11 +95,6 @@ export default {
         this.dealerCards.push(cardsPick.cards[0])
         this.dealerCards.push(cardsPick.cards[1])
       }
-      console.log(cardsPick.cards[0])
-      console.log(cardsPick.cards[1])
-    },
-    dealerTurn(){
-      console.log("Verify Cards");
     },
     chipsVerify(){
       if(this.betValue <= 0){
@@ -106,9 +105,62 @@ export default {
         alert("Fichas insuficientes") 
         return false
       }
+      return true;
     },
     chipsDiscount(){
       this.totalMoney -= this.betValue
+    },
+    verifyEndGame(){
+      if (this.valueDealerCards > 21)
+        return alert("Dealer Estourou.")
+      if(this.valuePlayerCards > 21)
+        return alert("Player Estourou.")
+    },
+
+    //DEALER
+
+    dealerTurn(){
+      console.log("dealerTurn");
+      this.dealerDeckCalculate()
+      if (this.valueDealerCards < 17){
+        this.dealerPickCard()
+        this.verifyEndGame()
+      }
+      else {
+        return console.log("Dealer Pass Turn")
+      }
+    },
+    dealerDeckCalculate(){
+      let valueCardOne = this.dealerCards[0].value
+      let valueCardTwo = this.dealerCards[1].value
+      if(valueCardOne == 'ACE'){
+        valueCardOne = 10
+      }else if (valueCardOne == 'JACK' || valueCardOne == 'QUEEN' || valueCardOne == 'KING'){
+        valueCardOne = 10
+      }
+      if(valueCardTwo == 'ACE'){
+        valueCardTwo = 10
+      }else if (valueCardTwo == 'JACK' || valueCardTwo == 'QUEEN' || valueCardTwo == 'KING'){
+        valueCardTwo = 10
+      }
+      this.valueDealerCards = parseInt(valueCardOne) + parseInt(valueCardTwo)
+      
+      console.log(`Dealer Deck Value: ${this.valueDealerCards}`);
+    },
+    async dealerPickCard(){
+      const url = `${this.baseUrl}${this.deckId}/draw/?count=1`
+      await axios.get(url)
+      .then(resp => {
+        this.dealerCards.push(resp.data.cards[0])
+      })
+      .catch(error => {
+        console.log("[ERROR] = " + error)
+        alert("Ops, algo de errado aconteceu.")
+      })
+      this.verifyEndGame()
+    },
+    generateRandomNumber(maxNumber){
+      return Math.floor(Math.random() * (maxNumber - 1 + 1) + 1)
     }
   }
 }
